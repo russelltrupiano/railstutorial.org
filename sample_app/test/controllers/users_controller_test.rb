9bd68db621acb5e35bb5f4775a9a9254f4ac9ae7
russelltrupiano <russelltrupiano@gmail.com>
2015-07-08 12:09:54 -0400

    ch9 done

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -1,9 +1,70 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  def setup
+    @user        = users(:russell)
+    @other_user  = users(:archer)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty? # Please log in msg
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty? # Please log in msg
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty? # Please log in msg
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty? # Please log in msg
+    assert_redirected_to root_url
+  end
+
+  test "should not allow admin attribute to be modified over the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @user, user: { password:              "foobar",
+                                      password_confirmation: "foobar",
+                                      admin: true }
+    assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+        delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in not as admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+        delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
   end
 
 end
